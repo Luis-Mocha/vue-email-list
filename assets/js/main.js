462b25c4ac2cba2ca51ad6b2e3baabe94614c46b
Luis-Mocha <luismocha97@gmail.com>
2023-04-25 17:29:30 +0200

    get single random email

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,24 @@ createApp({
 
     created() { 
         //cicli di vita dei componenti di applicazioni Vue. Vengono eseguite azioni al caricamento dei componenti
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (response) {
+            const result = response.data;
+            console.log(response, result);
+        });
         
     },
 
     methods: {
         // funzioni Vue3
+        
+        
          
     }
 }).mount('#app')
+
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
