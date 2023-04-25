3c3261f81abc9fb886bb8bd6b3e5d2bcd49553ad
Luis-Mocha <luismocha97@gmail.com>
2023-04-25 18:00:57 +0200

    showing email one at a time

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,30 @@ createApp({
     data() {
         return {
             // variabili Vue3
-             
+            email: {},
+            emailList: [],
         }
     },
 
     created() { 
         //cicli di vita dei componenti di applicazioni Vue. Vengono eseguite azioni al caricamento dei componenti
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (response) {
-            const result = response.data;
-            console.log(response, result);
-        });
+        
         
     },
 
     methods: {
         // funzioni Vue3
-        
-        
-         
+        getEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                let result = res.data.response;
+                console.log(res, result);
+                this.email = result;
+                console.log(this.email);
+
+                this.emailList.push(this.email)
+            });
+        }
     }
 }).mount('#app')
 
